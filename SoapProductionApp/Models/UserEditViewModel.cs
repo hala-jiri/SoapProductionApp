71f6e7e45ffa444a5c1c22f33f7811e340885681
Jiri Hala <hala.jiri@gmail.com>
2025-02-06 23:01:46 +0100

    Create UserEditViewModel

---
+++ SoapProductionApp/Models/UserEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SoapProductionApp.Models
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+        public List<string> AllRoles { get; set; }
+        public List<string> SelectedRoles { get; set; }
+    }
+}
