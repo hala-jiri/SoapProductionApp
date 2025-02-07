b1839fb872cf449f7c1b52379c7854450f3e1e6a
Jiri Hala <hala.jiri@gmail.com>
2025-02-07 21:58:32 +0100

    WarehouseItemBatch update

--- SoapProductionApp/Models/Warehouse/WarehouseItem.cs
+++ SoapProductionApp/Models/Warehouse/WarehouseItem.cs
@@ -36,15 +36,19 @@ namespace SoapProductionApp.Models.Warehouse
     {
         public int Id { get; set; }
 
+        [Required]
         public int WarehouseItemId { get; set; }
         public virtual WarehouseItem WarehouseItem { get; set; }
 
-        public decimal Quantity { get; set; } // Množství v tomto nákupu
+        [Required]
+        public decimal Quantity { get; set; } // Kolik bylo zakoupeno
 
-        public decimal PricePerUnit { get; set; } // Cena v tomto nákupu
+        [Required]
+        public decimal PricePerUnit { get; set; } // Cena v této objednávce
 
-        public DateTime? ExpirationDate { get; set; } // Datum expirace (může být null)
+        public DateTime? ExpirationDate { get; set; } // Expirace (nepovinná)
 
+        [Required]
         public DateTime PurchaseDate { get; set; } = DateTime.Now; // Datum nákupu
     }
 }
