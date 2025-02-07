e2c4f9f1f291839c4862ef3fedd2097cb02952e8
Jiri Hala <hala.jiri@gmail.com>
2025-02-07 21:53:49 +0100

    Warehouse view and controller

---
+++ SoapProductionApp/Controllers/WarehouseController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoapProductionApp.Data;
+using SoapProductionApp.Models;
+using SoapProductionApp.Models.Warehouse;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoapProductionApp.Controllers
+{
+    [Authorize]
+    public class WarehouseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WarehouseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _context.WarehouseItems.Include(i => i.Batches).ToListAsync();
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(WarehouseItem item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.WarehouseItems.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var item = await _context.WarehouseItems
+                .Include(i => i.Batches)
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (item == null)
+                return NotFound();
+
+            return View(item);
+        }
+    }
+}
