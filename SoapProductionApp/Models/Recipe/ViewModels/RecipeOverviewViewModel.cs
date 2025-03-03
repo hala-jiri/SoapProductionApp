8d076c7d3eff6b9f81a337ca41de4eae0c8403d6
Jiri Hala <hala.jiri@gmail.com>
2025-03-04 00:04:22 +0100

    Recipe view model and views

--- SoapProductionApp/Models/Recipe/ViewModels/RecipeOverviewViewModel.cs
+++ SoapProductionApp/Models/Recipe/ViewModels/RecipeOverviewViewModel.cs
@@ -1,12 +1,42 @@
-﻿namespace SoapProductionApp.Models.Recipe.ViewModels
+﻿using SoapProductionApp.Models.Warehouse;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoapProductionApp.Models.Recipe.ViewModels
 {
-    public class RecipeOverviewViewModel
+    /*public class RecipeOverviewViewModel
     {
-        /*public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; set; }
+
+
+
+        public string ImageUrl { get; set; }
+
+        [Required]
         public int BatchSize { get; set; }
-        public decimal TotalCost { get; set; }
-        public decimal CostPerPiece { get; set; }
-        public bool HasEnoughStock { get; set; }*/
+
+        [Required]
+        public int DaysOfCure { get; set; }
+
+        public List<RecipeIngredientViewModel> Ingredients { get; set; } = new List<RecipeIngredientViewModel>();
+
+        // Pro seznam skladových položek, aby si uživatel mohl vybírat ingredience
+        public List<RecipeIngredientDetailViewModel> AvailableWarehouseItems { get; set; }
+
+        public string? Note { get; set; }
     }
+
+    public class RecipeIngredientDetailViewModel
+    {
+        public int WarehouseItemId { get; set; }
+        public string WarehouseItemName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Quantity must be greater than zero.")]
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        public decimal Quantity { get; set; }
+
+        public string Unit { get; set; } // L, ml, kg, g, ks
+        public decimal Cost { get; set; }
+    
+    }*/
 }
