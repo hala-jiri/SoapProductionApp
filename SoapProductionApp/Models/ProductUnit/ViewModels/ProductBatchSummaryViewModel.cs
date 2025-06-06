1d5269971d50a0d527355de209127104c29be592
Jiri Hala <hala.jiri@gmail.com>
2025-06-07 00:31:08 +0200

    Update VM product batch summary

---
+++ SoapProductionApp/Models/ProductUnit/ViewModels/ProductBatchSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SoapProductionApp.Models.Recipe;
+
+namespace SoapProductionApp.Models.ProductUnit.ViewModels
+{
+    public class ProductBatchSummaryViewModel
+    {
+        public string RecipeName { get; set; }
+        public ProductType ProductType { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public decimal CostPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+        public int TotalUnits { get; set; }
+    }
+}
